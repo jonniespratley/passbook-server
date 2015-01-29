0179bf9603f11613adad4daacdaf8ff9ec898b7e
Jonnie Spratley <jonniespratley@gmail.com>
2015-01-29 09:08:17 -0800

    added routes

--- cmds/serve.js
+++ cmds/serve.js
@@ -4,13 +4,12 @@
 'use strict';
 
 module.exports = function(program) {
-
-	program
-		.command('serve')
-		.version('0.0.1')
-		.description('An express server for handling Passbook APIs')
-		.action(function(/* Args here */){
-			// Your code goes here
-		});
 	
+	program.command('serve').version('0.0.1').description('An express server for handling Passbook APIs').action(function(/* Args here */) {
+		// Your code goes here
+		program.log('info', 'Hello!');
+		program.info('Hello again');
+		program.debug('Can you hear me now?');
+	});
+
 };
--- test/passbook-server.js
+++ test/passbook-server.js
@@ -4,41 +4,41 @@ var assert = require("assert");
 var exec = require('child_process').exec;
 var path = require('path');
 
-describe('passbook-server bin', function(){
-	var cmd = 'node '+path.join(__dirname, '../bin/passbook-server')+' ';
+describe('passbook-server bin', function() {
+	var cmd = 'node ' + path.join(__dirname, '../bin/passbook-server') + ' ';
 	console.log(cmd);
 
 	it('--help should run without errors', function(done) {
-		exec(cmd+'--help', function (error, stdout, stderr) {
+		exec(cmd + '--help', function(error, stdout, stderr) {
 			assert(!error);
 			done();
 		});
 	});
 
 	it('--version should run without errors', function(done) {
-		exec(cmd+'--version', function (error, stdout, stderr) {
+		exec(cmd + '--version', function(error, stdout, stderr) {
 			assert(!error);
 			done();
 		});
 	});
 
 	it('should return error on missing command', function(done) {
-        this.timeout(4000);
+		this.timeout(4000);
 
-		exec(cmd, function (error, stdout, stderr) {
+		exec(cmd, function(error, stdout, stderr) {
 			assert(error);
-			assert.equal(error.code,1);
+			assert.equal(error.code, 1);
 			done();
 		});
 
 	});
 
 	it('should return error on unknown command', function(done) {
-        this.timeout(4000);
+		this.timeout(4000);
 
-		exec(cmd+'junkcmd', function (error, stdout, stderr) {
+		exec(cmd + 'junkcmd', function(error, stdout, stderr) {
 			assert(error);
-			assert.equal(error.code,1);
+			assert.equal(error.code, 1);
 			done();
 		});
 	});
