6318e0e4803728b5bc0c40341e34f57341433c5b
Jonnie Spratley <jonnie.spratley@ge.com>
2016-09-22 11:10:23 -0700

    fix: updated routes

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,59 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var gutil = require('gulp-util');
+'use strict';
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
 
-gulp.task('mocha', function() {
-	return gulp.src(['test/**/*-spec.js'], { read: false })
-	.pipe(mocha({ reporter: 'list' }))
-	.on('error', gutil.log);
+
+
+
+const config = {
+  src: [
+    'src/**/*.js'
+  ],
+  specs: 'test/specs/**/*-spec.js'
+};
+
+
+
+const jsdoc = require('gulp-jsdoc3');
+gulp.task('docs', function (cb) {
+    gulp.src(['README.md', './src/**/*.js'], {read: false})
+        .pipe(jsdoc(cb));
 });
 
-gulp.task('watch-mocha', function() {
-	gulp.watch(['lib/**', 'test/**'], ['mocha']);
+
+const coveralls = require('gulp-coveralls');
+gulp.task('coveralls', function() {
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls());
+});
+
+const istanbul = require('gulp-istanbul');
+gulp.task('pre-test', function() {
+  return gulp.src(config.src)
+    .pipe(istanbul({
+      includeUntested: true,
+    }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function() {
+  return gulp.src(config.specs)
+    .pipe(mocha({
+      read: false,
+      reporter:'mochawesome'
+    }))
+    .pipe(istanbul.writeReports())
+    .once('error', function() {
+      process.exit(1);
+    })
+    .once('end', function() {
+      process.exit();
+    });
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['src/**', 'test/specs/**'], ['test']);
 });
 
 
-gulp.task('default', ['mocha'], function(){
-
-});
+gulp.task('default', ['test', 'docs']);
--- src/routes/passes/passes-router.js
+++ src/routes/passes/passes-router.js
@@ -1,70 +1,57 @@
 'use strict';
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	Router = express.Router,
-	jsonParser = bodyParser.json();
-
+    bodyParser = require('body-parser'),
+    Router = express.Router,
+    jsonParser = bodyParser.json();
+const expressListRoutes = require('express-list-routes');
 const PassController = require('./passes-controller');
 
-module.exports = function(program, app) {
-	var logger = program.getLogger('router:passes');
-	var config = program.config.defaults;
+module.exports = function (program, app) {
+    var logger = program.getLogger('router:passes');
+    var config = program.config.defaults;
 
     const prefix = `/api/${config.version}/passes`;
     const adminPrefix = `/api/${config.version}/admin`;
+    const logPrefix = `/api/${config.version}/log`;
 
+    var passController = new PassController(program);
     var router = new Router();
-	var passController = new PassController(program);
-	var adminRouter = new Router();
-    app.get(`/api/${config.version}`, function(req, res){
+
+    var logRouter = new Router();
+    var adminRouter = new Router();
+
+
+    app.get(`/api/${config.version}`, function (req, res) {
         res.status(200).json({
             message: 'welcome'
         });
     });
-    adminRouter.get('/', function(req, res){
+    adminRouter.get('/', function (req, res) {
         res.status(200).json({
             message: 'welcome'
         });
     });
-    adminRouter.get('/passes', passController.get_all_passes);
+    adminRouter.get('/passes?', passController.get_all_passes);
     adminRouter.get('/passes/:id?', passController.get_pass);
     adminRouter.put('/passes/:id', jsonParser, passController.put_pass);
     adminRouter.post('/passes', jsonParser, passController.post_pass);
     adminRouter.delete('/passes/:id', passController.delete_pass);
 
-
-
-
-
-    	//Logging Endpoint
-    	app.post( '/log', jsonParser, function(req, res) {
-
-    		var data = {
-    			body : JSON.stringify(req.body),
-    			params : req.params,
-    			url : req.path,
-    			_id : 'log-' + Date.now().toString(),
-    			type: 'log',
-    			time : Date.now().toString()
-    		};
-    	program.db.put(data).then(function(msg) {
-    			res.status(200).send(msg);
-    		}, function(err) {
-    			res.status(400).send(err);
-    		});
-
-    	});
-
     router.get('/:pass_type_id/:serial_number', passController.get_passes);
+
+
+    //Logging Endpoint
+    logRouter.post('/', jsonParser, passController.post_log);
 
 
     app.use(prefix, router);
     app.use(adminPrefix, adminRouter);
+    app.use(logPrefix, logRouter);
+
+    expressListRoutes({prefix: adminPrefix}, 'API:', adminRouter);
+    expressListRoutes({prefix: logPrefix}, 'API:', logRouter);
+    expressListRoutes({prefix: prefix}, 'API:', router);
 
 
-
-    require('express-list-routes')({ prefix: prefix }, 'API:', router );
-    require('express-list-routes')({ prefix: adminPrefix }, 'API:', adminRouter );
-
-	return router;
+    return router;
 };
