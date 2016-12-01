c48dbc32810f0e2a30f58a52803065aa5af6411c
Jonnie Spratley <jonnie.spratley@ge.com>
2016-12-01 13:32:16 -0800

    fix: updated mongo test

--- test/mongotest.js
+++ test/mongotest.js
@@ -5,9 +5,11 @@ const fs = require('fs-extra');
 const mongojs = require('mongojs');
 
 // simple usage for a local db
-var db = mongojs('localhost/passbook-server');
+var endpoint = 'localhost/passbook-server';
 
-var passes = db.collection('passes');
+
+//var db = mongojs('localhost/passbook-server');
+//var passes = db.collection('passes');
 
 
 var mockObj = {
@@ -18,15 +20,82 @@ var mockObj = {
 
 // use the save function to just save a document (callback is optional for all writes)
 
+class MongoDbAdapter{
+  constructor(name, config){
+    this.name = name;
+    this.config = config;
+    this.db = mongojs(config.url);
+    this.collection = this.db.collection(name);
+  }
+
+  allDocs(params){
+    return this.find(params);
+  }
+
+  find(params){
+    return new Promise((resolve, reject) =>{
+      this.collection.find(params, (err, docs)=>{
+        if(err){
+          reject(err);
+        }
+        resolve(docs);
+      });
+    });
+  }
+  get(params){
+    return new Promise((resolve, reject) =>{
+      this.collection.findOne(params, (err, docs)=>{
+        if(err){
+          reject(err);
+        }
+        resolve(docs);
+      });
+    });
+  }
+
+  post(doc){
+    return this.put(doc);
+  }
+
+  put(doc){
+    return new Promise((resolve, reject) =>{
+      this.collection.save(doc, function(err, resp){
+        if(err){
+          reject(err);
+        }
+        resolve(resp);
+      });
+    });
+  }
+
+  remove(doc){
+    return new Promise((resolve, reject) =>{
+      this.collection.remove(doc, function(err, resp){
+        if(err){
+          reject(err);
+        }
+        resolve(resp);
+      });
+    });
+  }
+
+}
+
+
+
+
+var db = new MongoDbAdapter('passes', {url: endpoint});
 
 var doc;
-glob('test/temp/file-db/*.json', (err, files)=>{
+glob(__dirname + '/temp/file-db/*.json', (err, files)=>{
   console.log('Glob', err, files);
   files.forEach((file) =>{
     doc = fs.readJsonSync(file);
-    passes.save(doc, function(err, resp){
-      console.log('resp', err, resp);
-    });
+    if(doc.docType === 'pass'){
+      db.put(doc).then((res) =>{
+        console.log('Result', res);
+      });
+    }
   });
 
 });
